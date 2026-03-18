f2d0b9a1f81fba457840e0cad288faad64e6af81
BigHouse <1361981940@qq.com>
2026-03-18 11:28:49 +0800

    订单支付功能代码（不全，缺少两个文件，而且properties也没换成自己的）

--- sky-server/src/main/java/com/sky/controller/user/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/user/OrderController.java
@@ -1,15 +1,15 @@
 package com.sky.controller.user;
 
 
+import com.sky.dto.OrdersPaymentDTO;
 import com.sky.result.Result;
 import com.sky.service.OrderService;
 import com.sky.service.UserService;
+import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderSubmitVO;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.sky.dto.OrdersSubmitDTO;
 
 @RestController("userOrderController")
@@ -27,4 +27,20 @@ public class OrderController {
         OrderSubmitVO orderSubmitVO = orderService.submit(orderSubmitDTO);
         return Result.success(orderSubmitVO);
     }
+
+    /**
+     * 订单支付
+     *
+     * @param ordersPaymentDTO
+     * @return
+     */
+    @PutMapping("/payment")
+    @ApiOperation("订单支付")
+    public Result<OrderPaymentVO> payment(@RequestBody OrdersPaymentDTO ordersPaymentDTO) throws Exception {
+
+        OrderPaymentVO orderPaymentVO = orderService.payment(ordersPaymentDTO);
+
+        return Result.success(orderPaymentVO);
+    }
+
 }
